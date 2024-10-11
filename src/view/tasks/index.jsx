f67ec3972a9f849001e0c939cccbacb324938a30
rudy-pawnfi <jiawang.ouyang@pawnfi.io>
2024-10-11 11:28:25 +0800

    fix build

--- src/view/tasks/index.jsx
+++ src/view/tasks/index.jsx
@@ -372,7 +372,7 @@ const TasksPage = () => {
                                             <img className="mr_5" src={imgicon_2} alt="" srcSet="" />
                                             <div>
                                                 <div className="fs_2 fw_m">Login to your account daily</div>
-                                                <div className="fs_2 text_4">+90 BP</div>
+                                                <div className="fs_2 text_4">+200 BP</div>
                                             </div>
                                         </div>
                                         {
